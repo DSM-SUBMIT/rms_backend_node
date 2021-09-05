cbd1165cb317ff1cdcf56e933456118e82f37051
pjp0704 <junhyeong25@gmail.com>
2021-09-05 20:36:40 +0900

    [FEAT] Typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: false,
+      logging: Boolean(process.env.DB_LOGGING),
     }),
   ],
   controllers: [],
