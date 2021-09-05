f8d85f49d02dce16038a35f3670325f8392def00
pjp0704 <junhyeong25@gmail.com>
2021-09-05 19:31:44 +0900

    [FEAT] Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('RMS API document')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('apidocs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
