cc151c75e2417a5b1c3cc862a82fc61212759bc2
pjp0704 <junhyeong25@gmail.com>
2021-09-05 21:04:48 +0900

    [ADD] password hashing, comparing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { Admin } from './entities/admin.entity';
 
 @Injectable()
@@ -9,4 +10,14 @@ export class AuthService {
     @InjectRepository(Admin)
     private readonly adminsRepository: Repository<Admin>,
   ) {}
+
+  async encrypt(password: string): Promise<string> {
+    return await bcrypt.hash(password, 12);
+  }
+
+  async decrypt(id: string, password: string): Promise<boolean> {
+    const res = await this.adminsRepository.findOne(id);
+    if (!res) return false;
+    return await bcrypt.compare(password, res.password);
+  }
 }
