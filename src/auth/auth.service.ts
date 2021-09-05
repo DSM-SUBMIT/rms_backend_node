115778a312adf58963d59a67875f24e78eb2ae1e
pjp0704 <junhyeong25@gmail.com>
2021-09-05 20:48:03 +0900

    [ADD] Admin repository injection

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Admin } from './entities/admin.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(Admin)
+    private readonly adminsRepository: Repository<Admin>,
+  ) {}
+}
