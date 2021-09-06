c13e9c1b6fb59fe075b3cd8024e88a888cde6798
pjp0704 <junhyeong25@gmail.com>
2021-09-06 22:43:45 +0900

    [UPDATE] example property for swagger

--- src/auth/dto/request/login.dto.ts
+++ src/auth/dto/request/login.dto.ts
@@ -4,12 +4,14 @@ import { IsString } from 'class-validator';
 export class LoginDto {
   @ApiProperty({
     description: '어드민 계정의 아이디',
+    example: 'testid',
   })
   @IsString()
   id: string;
 
   @ApiProperty({
     description: '어드민 계정의 비밀번호',
+    example: 'testpw1234!#',
   })
   @IsString()
   password: string;
