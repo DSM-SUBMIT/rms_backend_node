49502d60080adf4da68470abc3cf68dc766ae844
Spectre_ <junhyeong25@gmail.com>
2021-09-07 18:11:00 +0900

    [CHORE] cherry-pick auth commits
    # Commit body
    
    
    # --- COMMIT PREFIX ---
    # [FEAT] : 새로운 기능 추가
    # [FIX] : 버그 수정(바르지 않은 동작을 고친 경우)
    # [REFACTOR] : 코드 전면 수정(기능은 변하지않음)
    # [CHANGE]: 코드 수정
    # [ADD] : 코드, 테스트, 예제, 문서, 이미지 등의 추가
    # [REMOVE] : 코드의 삭제
    # [STYLE] : 코드 형식, 정렬, 주석등의 변경
    # [DOCS]: 문서 수정
    # [TEST] : 테스트 추가
    # [CHORE]: 위에 해당되지않는 모든 변경
    # [FILE]: 파일의 위치 변경
    # [DELETE] : 파일 삭제
    # [INIT] : 기본 파일 추가
    
    # 동명사보다 명사를 사용하자.
    # 관사는 꼭 필요한 경우가 아니면 사용하지 않는다.
    # 명령문 형태로 작성한다.
    #	부정문 Don't를 사용한다.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { UseGuards } from '@nestjs/common';
 import { HttpCode } from '@nestjs/common';
 import { Controller, Post, Body, Patch, Request } from '@nestjs/common';
 import {
+  ApiBearerAuth,
   ApiConflictResponse,
   ApiCreatedResponse,
   ApiNoContentResponse,
@@ -9,9 +10,12 @@ import {
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { Roles } from '../utils/decorators/roles.decorator';
 import { ChangePwDto } from './dto/request/changePw.dto';
 import { LoginDto } from './dto/request/login.dto';
-import { JwtAuthGuard } from './jwt-auth.guard';
+import { Role } from '../utils/enums/role.enum';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -29,8 +33,11 @@ export class AuthController {
 
   @Patch('password')
   @HttpCode(204)
+  @Roles(Role.Admin)
+  @UseGuards(RolesGuard)
   @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: '어드민 비밀번호 변경' })
+  @ApiBearerAuth()
   @ApiNoContentResponse({
     description: '요청이 성공적으로 완료되었으나 응답 본문이 존재하지 않음',
   })
