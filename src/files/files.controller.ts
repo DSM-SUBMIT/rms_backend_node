f3f48492ee548eba258930376aa2c43a523705a3
pjp0704 <junhyeong25@gmail.com>
2021-09-08 23:16:14 +0900

    [FEAT] upload images to S3

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Request,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../utils/decorators/roles.decorator';
+import { Role } from '../utils/enums/role.enum';
 import { FilesService } from './files.service';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Post('images')
+  @Roles(Role.User)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FilesInterceptor('images'))
+  @ApiOperation({ summary: '이미지 파일 업로드' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        files: {
+          type: 'array',
+          items: {
+            type: 'string',
+            format: 'binary',
+          },
+        },
+      },
+    },
+  })
+  @ApiBearerAuth()
+  @ApiCreatedResponse({
+    description: '요청이 성공적으로 완료되어 리소스가 생성됨',
+  })
+  @ApiUnauthorizedResponse({ description: '토큰이 올바르지 않음' })
+  @ApiForbiddenResponse({ description: '권한이 존재하지 않음' })
+  uploadImages(
+    @UploadedFiles() files: Express.MulterS3.File[],
+    @Request() req,
+  ) {
+    return this.filesService.uploadImages(files, `${req.user.userId}`);
+  }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+import { extname } from 'path';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  async uploadImages(files: Express.MulterS3.File[], folder: string) {
+    const locations = new Array<string>();
+    const extCheck = files.map((file) => {
+      const ext = extname(file.originalname).toLowerCase();
+      const regex = new RegExp(/(jpg)|(png)|(jpeg)/);
+      if (!regex.test(ext)) {
+        throw new HttpException(
+          `This file extension(${ext}) is not supported.`,
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    });
+    await Promise.all(extCheck);
+
+    const uploadToS3 = files.map((file) => {
+      const bucketS3 = process.env.AWS_S3_BUCKET;
+      const filename = uuid();
+      const ext = extname(file.originalname).toLowerCase();
+
+      locations.push(
+        `https://${bucketS3}.s3.${process.env.AWS_REGION}.amazonaws.com/${folder}/${filename}
+              ${ext}`,
+      );
+      return this.uploadS3(
+        file.buffer,
+        `${bucketS3}/${folder}`,
+        filename + ext,
+      );
+    });
+    try {
+      await Promise.all(uploadToS3);
+    } catch {
+      throw new HttpException(
+        'An error has occured.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    return { uploaded: true, urls: locations };
+  }
+
+  async uploadS3(file, bucket, name) {
+    const s3 = this.getS3();
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+    };
+
+    return new Promise((resolve, reject) => {
+      s3.upload(params, (err, data) => {
+        if (err) {
+          Logger.error(err);
+          reject(err.message);
+        }
+        resolve(data);
+      });
+    });
+  }
+
+  getS3() {
+    return new S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    });
+  }
+}
