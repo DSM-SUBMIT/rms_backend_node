2555e9c5a7dfbe439b11eba1b53f2dc8049914f8
pjp0704 <junhyeong25@gmail.com>
2021-09-09 18:02:04 +0900

    [ADD] projects service

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Project } from './entities/project.entity';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(private readonly projectsRepository: Repository<Project>) {}
+
+  async getProject(id: number): Promise<Project> {
+    return await this.projectsRepository.findOne(id);
+  }
+}
