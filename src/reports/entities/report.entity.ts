d4057341d6cb568a42f43d6efa3b0554915e361e
pjp0704 <junhyeong25@gmail.com>
2021-09-09 13:23:39 +0900

    [ADD] report entity

---
+++ src/reports/entities/report.entity.ts
@@ -0,0 +1,25 @@
+import { Project } from '../../projects/entities/project.entity';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+
+@Entity()
+export class Report {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  @OneToOne((type) => Project)
+  @JoinColumn({ name: 'project_id' })
+  projectId: string;
+
+  @Column({ name: 'video_url', type: 'varchar', length: 256, nullable: true })
+  videoUrl: string;
+
+  @Column({ name: 'pdf_url', type: 'varchar', length: 256, nullable: true })
+  pdfUrl: string;
+
+  @Column({ name: 'start_date', type: 'char', length: 7, nullable: false })
+  startDate: string;
+
+  @Column({ name: 'end_date', type: 'char', length: 7, nullable: false })
+  endDate: string;
+
+  @Column({ type: 'varchar', length: 20000, nullable: false })
+  content: string;
+}
