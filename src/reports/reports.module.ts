3adedc05c75e8d003125c91a820d51b6bf3f6bdd
pjp0704 <junhyeong25@gmail.com>
2021-09-09 17:55:28 +0900

    [ADD] reports service

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 
 @Module({
-  controllers: [],
   providers: [ReportsService],
+  exports: [ReportsService],
 })
 export class ReportsModule {}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Report } from './entities/report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(private readonly reportsRepository: Repository<Report>) {}
+
+  async getReport(id: number) {
+    return await this.reportsRepository.findOne(id);
+  }
+}
