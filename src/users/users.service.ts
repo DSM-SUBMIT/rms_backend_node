a26fbc93b27c202eba337d47caee75582b0984fe
pjp0704 <junhyeong25@gmail.com>
2021-09-09 16:59:36 +0900

    [ADD] export UsersSevice

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly usersRepository: Repository<User>) {}
+
+  async getUserById(id: number): Promise<User> {
+    return await this.usersRepository.findOne(id);
+  }
+}
