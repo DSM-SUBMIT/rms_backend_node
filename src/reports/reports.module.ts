d2231c9d9ec132c1481cb31330ce4dee2420897a
pjp0704 <junhyeong25@gmail.com>
2021-09-09 18:01:41 +0900

    [ADD] missing typeorm imports

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './entities/report.entity';
 import { ReportsService } from './reports.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Report])],
   providers: [ReportsService],
   exports: [ReportsService],
 })
