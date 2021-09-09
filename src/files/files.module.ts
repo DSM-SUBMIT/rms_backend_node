822ab83002bafb2b20ebe38c627fd2ce87c4b494
pjp0704 <junhyeong25@gmail.com>
2021-09-09 19:26:30 +0900

    [ADD] import PlansModule

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { PlansModule } from '../shared/plans/plans.module';
 
 @Module({
+  imports: [PlansModule],
   controllers: [FilesController],
   providers: [FilesService],
 })
