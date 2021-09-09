678901a54146a5014e8fdb4e0fe0fcf856710210
pjp0704 <junhyeong25@gmail.com>
2021-09-09 19:26:12 +0900

    [ADD] PlansService

--- src/shared/plans/plans.service.ts
+++ src/shared/plans/plans.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Plan } from './entities/plan.entity';
 
 @Injectable()
-export class PlansService {}
+export class PlansService {
+  constructor(
+    @InjectRepository(Plan) private readonly plansRepository: Repository<Plan>,
+  ) {}
+
+  async getPlanById(id: number): Promise<Plan> {
+    return await this.plansRepository.findOne(id);
+  }
+
+  async updatePdfUrl(id: number, url: string): Promise<boolean> {
+    const res = await this.plansRepository.update(id, { pdfUrl: url });
+    return res ? true : false;
+  }
+}
