2fb2eca895a2de439d181f3838dc4d3a35978f7b
pjp0704 <junhyeong25@gmail.com>
2021-09-12 00:39:57 +0900

    [ADD] getStatusById(), updatePlanAccepted()

--- src/shared/status/status.service.ts
+++ src/shared/status/status.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Status } from './entities/status.entity';
 
 @Injectable()
-export class StatusService {}
+export class StatusService {
+  constructor(
+    @InjectRepository(Status)
+    private readonly statusRepository: Repository<Status>,
+  ) {}
+
+  async getStatusById(id: number): Promise<Status> {
+    return await this.statusRepository.findOne(id);
+  }
+
+  async updatePlanAccepted(id: number, status: boolean): Promise<boolean> {
+    const res = await this.statusRepository.update(id, {
+      isPlanAccepted: status,
+    });
+    return res ? true : false;
+  }
+}
