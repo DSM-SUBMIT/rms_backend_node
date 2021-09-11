4735b8d0537e7f14fdae409427a8233ca40cb6de
pjp0704 <junhyeong25@gmail.com>
2021-09-12 03:30:38 +0900

    [TEST] tests for confirmProject()

--- src/projects/projects.service.spec.ts
+++ src/projects/projects.service.spec.ts
@@ -1,3 +1,8 @@
+import {
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { StatusService } from 'src/shared/status/status.service';
@@ -5,17 +10,63 @@ import { Repository } from 'typeorm';
 import { Project } from './entities/project.entity';
 import { ProjectsService } from './projects.service';
 
+const mockStatus = [
+  {},
+  {
+    projectId: 1,
+    isPlanSubmitted: true,
+    isReportSubmitted: true,
+    planSubmittedAt: null,
+    reportSubmittedAt: null,
+    isPlanAccepted: null,
+    isReportAccepted: null,
+  },
+  {},
+  {
+    projectId: 3,
+    isPlanSubmitted: false,
+    isReportSubmitted: false,
+    planSubmittedAt: null,
+    reportSubmittedAt: null,
+    isPlanAccepted: null,
+    isReportAccepted: null,
+  },
+  {
+    projectId: 4,
+    isPlanSubmitted: true,
+    isReportSubmitted: true,
+    planSubmittedAt: null,
+    reportSubmittedAt: null,
+    isPlanAccepted: true,
+    isReportAccepted: true,
+  },
+  {
+    projectId: 5,
+    isPlanSubmitted: true,
+    isReportSubmitted: true,
+    planSubmittedAt: null,
+    reportSubmittedAt: null,
+    isPlanAccepted: false,
+    isReportAccepted: false,
+  },
+];
+
 const mockProjectsRepository = () => ({
   findOne: jest.fn(),
 });
 const mockStatusService = () => ({
-  getStatusById: jest.fn(),
+  getStatusById: jest.fn().mockImplementation(async (id) => {
+    if (id !== mockStatus[id].projectId) return undefined;
+    return mockStatus[id];
+  }),
+  updatePlanAccepted: jest.fn(),
 });
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
+  let statusService: StatusService;
   let projectsRepository: MockRepository<Project>;
 
   beforeEach(async () => {
@@ -34,6 +85,7 @@ describe('ProjectsService', () => {
     }).compile();
 
     service = module.get<ProjectsService>(ProjectsService);
+    statusService = module.get<StatusService>(StatusService);
     projectsRepository = module.get<MockRepository<Project>>(
       getRepositoryToken(Project),
     );
@@ -42,6 +94,63 @@ describe('ProjectsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('confirmProject', () => {
+    describe('plan', () => {
+      it('should return nothing', async () => {
+        const result = await service.confirmProject(1, 'plan', {
+          type: 'approve',
+          comment: 'test',
+        });
+
+        expect(statusService.getStatusById).toHaveBeenCalledTimes(1);
+        expect(statusService.updatePlanAccepted).toHaveBeenCalledTimes(1);
+
+        expect(statusService.getStatusById).toHaveBeenCalledWith(1);
+        expect(statusService.updatePlanAccepted).toHaveBeenCalledWith(1, true);
+
+        expect(result).toBeUndefined();
+      });
+      it('should throw NotFoundException', () => {
+        expect(
+          service.confirmProject(2, 'plan', {
+            type: 'approve',
+            comment: 'test',
+          }),
+        ).rejects.toThrow(NotFoundException);
+      });
+      it('should throw ConflictException', () => {
+        expect(
+          service.confirmProject(3, 'plan', {
+            type: 'approve',
+            comment: 'test',
+          }),
+        ).rejects.toThrow(ConflictException);
+        expect(
+          service.confirmProject(4, 'plan', {
+            type: 'approve',
+            comment: 'test',
+          }),
+        ).rejects.toThrow(ConflictException);
+        expect(
+          service.confirmProject(5, 'plan', {
+            type: 'approve',
+            comment: 'test',
+          }),
+        ).rejects.toThrow(ConflictException);
+      });
+    });
+
+    it('should throw BadRequestException', () => {
+      expect(
+        service.confirmProject(1, 'this will throw error', {
+          type: 'approve',
+          comment: '',
+        }),
+      ).rejects.toThrow(BadRequestException);
+    });
+  });
+
   describe('getProject', () => {
     it('should return undefined', async () => {
       projectsRepository.findOne.mockResolvedValue(undefined);
