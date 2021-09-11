0834675bd2bd44d685d30dd49e33303754bb33de
pjp0704 <junhyeong25@gmail.com>
2021-09-12 00:39:36 +0900

    [ADD] imports TypeOrmModule, exports StatusService

--- src/shared/status/status.module.ts
+++ src/shared/status/status.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Status } from './entities/status.entity';
 import { StatusService } from './status.service';
 
 @Module({
-  controllers: [],
+  imports: [TypeOrmModule.forFeature([Status])],
   providers: [StatusService],
+  exports: [StatusService],
 })
 export class StatusModule {}
