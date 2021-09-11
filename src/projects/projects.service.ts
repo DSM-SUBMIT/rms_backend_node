8dda78fe1510614ccbfb7fcb74f3fd3783a03e3b
pjp0704 <junhyeong25@gmail.com>
2021-09-12 03:52:09 +0900

    [FEAT] confirm submitted report

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -47,6 +47,28 @@ export class ProjectsService {
         }
         return;
       }
+      case 'report': {
+        const status = await this.statusService.getStatusById(projectId);
+        if (!status) throw new NotFoundException();
+        if (
+          !status.isReportSubmitted ||
+          status.isReportAccepted ||
+          (status.isReportAccepted !== null && conflictCheck)
+        )
+          throw new ConflictException();
+
+        switch (payload.type) {
+          case 'approve': {
+            await this.statusService.updateReportAccepted(projectId, true);
+            break;
+          }
+          case 'deny': {
+            await this.statusService.updateReportAccepted(projectId, false);
+            break;
+          }
+        }
+        return;
+      }
       default: {
         throw new BadRequestException();
       }
