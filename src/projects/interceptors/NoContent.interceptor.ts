6c26312711d589993f652bc8393318e221ad85e4
pjp0704 <junhyeong25@gmail.com>
2021-09-12 11:13:08 +0900

    [ADD] NoContentInterceptor to return 204 when response body is empty

---
+++ src/projects/interceptors/NoContent.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class NoContentInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+
+    return next.handle().pipe(
+      tap((data) => {
+        if (data === undefined) response.status(204);
+      }),
+    );
+  }
+}
