8a3d7ceebd0c0e001dff8d7a9b24b1ebb043e099
pjp0704 <junhyeong25@gmail.com>
2021-09-12 15:01:30 +0900

    [FIX] unchanged import statement

--- src/projects/dto/response/projectsList.dto.ts
+++ src/projects/dto/response/projectsList.dto.ts
@@ -1,4 +1,4 @@
-import { ProjectItem } from 'src/projects/interfaces/project.interface';
+import { ProjectItem } from 'src/projects/interfaces/projectItem.interface';
 
 export class ProjectsListDto {
   order_by?: 'plan' | 'report';
