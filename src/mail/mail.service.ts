cbf90aa6897c8a89dac485dfff56198ad2e553c3
pjp0704 <junhyeong25@gmail.com>
2021-09-16 07:21:41 +0900

    [ADD] sendMail method

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  async sendMail(
+    receiver: string,
+    subject: string,
+    template: string,
+    context: any = {},
+  ) {
+    try {
+      await this.mailerService.sendMail({
+        to: receiver,
+        from: process.env.OAUTH_USER,
+        subject,
+        template: `./${template}`,
+        context,
+      });
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+}
