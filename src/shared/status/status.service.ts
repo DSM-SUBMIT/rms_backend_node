2faca232421c0010b6073afe2a6378dd3ef59ebf
pjp0704 <junhyeong25@gmail.com>
2021-09-15 10:24:02 +0900

    [FIX] where option

--- src/shared/status/status.service.ts
+++ src/shared/status/status.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { IsNull, Not, Repository } from 'typeorm';
+import { IsNull, Repository } from 'typeorm';
 import { Status } from './entities/status.entity';
 
 @Injectable()
@@ -17,9 +17,12 @@ export class StatusService {
   async getStatusDescByPlanDate(
     limit: number,
     page: number,
-  ): Promise<Status[]> {
-    return await this.statusRepository.find({
-      where: { planSubmittedAt: Not(IsNull()) },
+  ): Promise<[Status[], number]> {
+    return await this.statusRepository.findAndCount({
+      where: {
+        isPlanSubmitted: true,
+        isPlanAccepted: IsNull(),
+      },
       order: {
         planSubmittedAt: 'ASC',
       },
@@ -35,9 +38,24 @@ export class StatusService {
   async getStatusDescByReportDate(
     limit: number,
     page: number,
-  ): Promise<Status[]> {
-    return await this.statusRepository.find({
-      where: { reportSubmittedAt: Not(IsNull()) },
+  ): Promise<[Status[], number]> {
+    return await this.statusRepository.findAndCount({
+      where: {
+        isReportSubmitted: true,
+        isReportAccepted: IsNull(),
+      },
+      order: {
+        reportSubmittedAt: 'ASC',
+      },
+      take: limit,
+      skip: limit * (page - 1),
+      relations: [
+        'projectId',
+        'projectId.projectField',
+        'projectId.projectField.fieldId',
+      ],
+    });
+  }
       order: {
         planSubmittedAt: 'ASC',
       },
