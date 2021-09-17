3b2c1336cbcb261c93229918323fdea46c00fe7e
pjp0704 <junhyeong25@gmail.com>
2021-09-17 21:19:36 +0900

    [TEST] mock service

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailService } from './mail.service';
+import { MailerService } from '@nestjs-modules/mailer';
+jest.mock('@nestjs-modules/mailer');
 
 describe('MailService', () => {
   let service: MailService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MailService],
+      providers: [MailerService, MailService],
     }).compile();
 
     service = module.get<MailService>(MailService);
