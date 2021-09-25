23f21740f9000da53eabac6a2c5a1e798b4b0c7f
pjp0704 <junhyeong25@gmail.com>
2021-09-25 22:38:50 +0900

    [TEST] tests for uploadImages in FilesController

--- src/files/files.controller.spec.ts
+++ src/files/files.controller.spec.ts
@@ -1,49 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PlansService } from 'src/shared/plans/plans.service';
-import { ReportsService } from 'src/shared/reports/reports.service';
-import { UsersService } from 'src/shared/users/users.service';
+import { Request } from 'express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+jest.mock('./files.service');
 
-const mockPlansService = () => ({
-  getPlanById: jest.fn(),
-  updatePdfUrl: jest.fn(),
-});
-const mockReportsService = () => ({
-  getReportById: jest.fn(),
-  updatePdfUrl: jest.fn(),
-});
-const mockUsersService = () => ({
-  getUserById: jest.fn(),
-});
+const mockFiles: Express.MulterS3.File[] = [];
+const mockRequest: Partial<Request> = { user: { userId: 1 } };
 
 describe('FilesController', () => {
   let controller: FilesController;
+  let service: FilesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FilesController],
-      providers: [
-        {
-          provide: PlansService,
-          useValue: mockPlansService(),
-        },
-        {
-          provide: ReportsService,
-          useValue: mockReportsService(),
-        },
-        {
-          provide: UsersService,
-          useValue: mockUsersService(),
-        },
-        FilesService,
-      ],
+      providers: [FilesService],
     }).compile();
 
     controller = module.get<FilesController>(FilesController);
+    service = module.get<FilesService>(FilesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('uploadImages', () => {
+    it('should ensure guards are applied', () => {
+      const guards = Reflect.getMetadata(
+        '__guards__',
+        FilesController.prototype.uploadImages,
+      );
+      const jwtAuthGuard = new guards[0]();
+      const rolesGuard = new guards[1]();
+
+      expect(jwtAuthGuard).toBeInstanceOf(JwtAuthGuard);
+      expect(rolesGuard).toBeInstanceOf(RolesGuard);
+    });
+    it('should ensure user role is applied', () => {
+      const roles = Reflect.getMetadata(
+        'roles',
+        FilesController.prototype.uploadImages,
+      );
+      const role = roles[0];
+
+      expect(role).toEqual('user');
+    });
+    it('should return urls', () => {
+      controller.uploadImages(mockFiles, mockRequest, 1);
+      expect(service.uploadImages).toHaveBeenCalled();
+      expect(service.uploadImages).toHaveBeenCalledWith(mockFiles, 1, 1);
+    });
   });
 });
