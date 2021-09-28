81f5eafac95fcf98fa4608bd5c3b9fe3f7e3d982
pjp0704 <junhyeong25@gmail.com>
2021-09-28 18:14:09 +0900

    [TEST] check the guards are applied

--- src/projects/projects.controller.spec.ts
+++ src/projects/projects.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 
@@ -14,6 +16,15 @@ describe('ProjectsController', () => {
     }).compile();
 
     controller = module.get<ProjectsController>(ProjectsController);
+  });
+
+  it('should ensure the guards is applied to the controller', async () => {
+    const guards = Reflect.getMetadata('__guards__', ProjectsController);
+    const jwtAuthGuard = new guards[0]();
+    const rolesGuard = new guards[1]();
+
+    expect(jwtAuthGuard).toBeInstanceOf(JwtAuthGuard);
+    expect(rolesGuard).toBeInstanceOf(RolesGuard);
   });
 
   it('should be defined', () => {
