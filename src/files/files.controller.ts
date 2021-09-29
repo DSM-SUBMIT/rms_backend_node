af35fbe788113019c9fad0ec44e5726a898a9849
pjp0704 <junhyeong25@gmail.com>
2021-09-29 09:21:22 +0900

    [FIX] missing host option

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,7 +3,6 @@ import {
   Get,
   Param,
   Request,
-  StreamableFile,
   UploadedFile,
   UploadedFiles,
   UseInterceptors,
@@ -30,7 +29,7 @@ import { Roles } from '../utils/decorators/roles.decorator';
 import { Role } from '../utils/enums/role.enum';
 import { FilesService } from './files.service';
 
-@Controller('files')
+@Controller({ host: 'files-api.dsm-rms.com', path: 'files' })
 @ApiTags('파일 API')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
