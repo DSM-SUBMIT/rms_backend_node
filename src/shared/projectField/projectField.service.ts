ad4810360b0dbd5335bc0eec4f652e22f350f7cb
pjp0704 <junhyeong25@gmail.com>
2021-10-05 02:51:41 +0900

    [STYLE] rename ProjectFieldsService

--- src/shared/projectField/projectField.service.ts
+++ src/shared/projectField/projectField.service.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import { ProjectField } from './entities/projectField.entity';
 
 @Injectable()
-export class ProjectFieldsService {
+export class ProjectFieldService {
   constructor(
     @InjectRepository(ProjectField)
     private readonly projectFieldRepository: Repository<ProjectField>,
