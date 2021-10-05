5bbd42fc97d0b89bb560af4baf672f9143960f0e
pjp0704 <junhyeong25@gmail.com>
2021-10-05 19:48:08 +0900

    [ADD] import 추가

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -3,9 +3,10 @@ import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
 import { ReportsModule } from 'src/shared/reports/reports.module';
 import { ProjectsModule } from 'src/projects/projects.module';
+import { StatusModule } from 'src/shared/status/status.module';
 
 @Module({
-  imports: [ProjectsModule, ReportsModule],
+  imports: [ProjectsModule, ReportsModule, StatusModule],
   controllers: [FilesController],
   providers: [FilesService],
 })
