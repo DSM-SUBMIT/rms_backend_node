63ac815914b62bd94d3a405b9f1ffc6abadf2510
pjp0704 <junhyeong25@gmail.com>
2021-10-14 18:43:01 +0900

    [FEAT] DB health check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
+import {
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+  HealthCheck,
+} from '@nestjs/terminus';
 
 @ApiExcludeController()
 @Controller('health')
 export class HealthController {
-  constructor() {}
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly db: TypeOrmHealthIndicator,
+  ) {}
 
   @Get()
+  @HealthCheck()
   healthCheck() {
-    return 'OK';
+    return this.health.check([() => this.db.pingCheck('database')]);
   }
 }
